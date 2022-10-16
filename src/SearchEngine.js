572abf390838978182ecdc01f9ac3ae53d9cfcb2
GitMaiHub <h.maibcarlton.i@gmail.com>
2022-10-16 14:48:13 -0600

    Added a form to SearchEngine.js.

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import "./SearchEngine.css";
 export default function SearchEngine(props) {
     return (
         <div className="SearchEngine">
-            
+            <form>
+                <div className="row">
+<div className="col-md-9">
+    <input type="search" placeholder="Enter a city..." className="form-control" />
+</div>
+<div className="col-md-3 d-grid d-md-block">
+    <input type="submit" value="Search" className="btn btn-success" />
+</div>
+
+                </div>
+            </form>
         </div>
     )
 }
