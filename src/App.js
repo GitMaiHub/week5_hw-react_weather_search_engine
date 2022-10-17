20f169171fdd4327d9aab132f015ce5799bc47f2
GitMaiHub <h.maibcarlton.i@gmail.com>
2022-10-17 15:10:50 -0600

    Updated Netlify link.

--- src/App.js
+++ src/App.js
@@ -11,10 +11,38 @@ export default function App() {
         <SearchEngine defaultCity="Seattle" />
       </div>
       <footer>
-        Open-sourced on <a href="https://github.com/GitMaiHub/week5_hw-react_weather_search_engine" title="View this project on GitHub(external" target="_blank" rel= "noreferrer" className="links">GitHub</a> and hosted on <a href="https://app.netlify.com/sites/tiny-trifle-5ef3ce/overview" title="View this project on Netlify(external)" target="_blank" rel="noreferrer" className="links">Netlify</a>
+        Open-sourced on{" "}
+        <a
+          href="https://github.com/GitMaiHub/week5_hw-react_weather_search_engine"
+          title="View this project on GitHub(external"
+          target="_blank"
+          rel="noreferrer"
+          className="links"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://app.netlify.com/sites/tiny-trifle-5ef3ce/overview"
+          title="View this project on Netlify(external)"
+          target="_blank"
+          rel="noreferrer"
+          className="links"
+        >
+          Netlify
+        </a>
         <p>
           <img src={maicon} alt="maicon" width={40} />
-        Coded by <a href="https://frolicking-dieffenbachia-8704e5.netlify.app" title="Mai's portfolio" target="_blank" rel="noreferrer" className="links">Mai Carlton</a>
+          Coded by{" "}
+          <a
+            href="https://glittery-salamander-9de934.netlify.app/"
+            title="Mai's portfolio"
+            target="_blank"
+            rel="noreferrer"
+            className="links"
+          >
+            Mai Carlton
+          </a>
         </p>
       </footer>
     </div>
